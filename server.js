1c0f36c9179dcdf5fa520e32eaf0ec57ee64676d
ShotaKiko <Shota.Kikozashvili@gmail.com>
2019-05-18 12:15:20 -0500

    Added get endpoint for projects

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const server = express()
 
+const db = require('./data/helpers/projectModel.js')
+const { get } = db
 
 //Middleware
 server.use(express.json())
@@ -11,5 +13,19 @@ server.get('/', (req, res) => {
       <p>You can do it</p>
       `);
   });
+
+server.get('/projects', async (req, res) =>{
+    try{
+        const projectList = await get();
+        res.status(200).json(projectList)
+    } catch (error) {
+        res.status(500).json({
+            message: "The project list could not be retrieved."
+        })
+    }
+})
+
+
+
 
   module.exports = server;
